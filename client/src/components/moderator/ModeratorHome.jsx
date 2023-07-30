1a05abf3c02ef72c3e22c6a0e67b1fb07012264f
Rahul C V <rahulcv447@gmail.com>
2023-07-30 11:39:29 +0530

    updated

---
+++ client/src/components/moderator/ModeratorHome.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ModeratorHeader from '../Header/ModeratorHeader'
+
+const ModeratorHome = () => {
+  return (
+   <>
+    <ModeratorHeader/>
+     <div style={{paddingTop:'100px'}}>
+      <h2>Moderator home</h2>
+    </div>
+   </>
+  )
+}
+
+export default ModeratorHome

4f13a4956a8e7f5f18778bd136d29bd860d906b0
Rahul C V <rahulcv447@gmail.com>
2023-07-30 11:12:42 +0530

    up

---
+++ server/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+// router
+const userRegRoutes = require('./router/UserRouter');
+
+const app = new express();
+
+
+// database
+mongoose.connect("mongodb+srv://abinseb09:abinseb09@cluster0.2xp4bal.mongodb.net/QuizzAppDB?retryWrites=true&w=majority")
+.then(()=>{
+    console.log("Database Connected");
+})
+.catch(err=>console.log(err));
+
+// midleware
+app.use(express.urlencoded({extended:true}))
+app.use(express.json());
+app.use(cors());
+
+// route
+app.use(userRegRoutes);
+
+const tagRouter=require('./router/addtags')
+app.use('/',tagRouter);
+
+// port
+app.listen(4010,()=>{
+    console.log("App is running in port 4010");
+})
